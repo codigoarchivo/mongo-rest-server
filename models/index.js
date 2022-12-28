e61b0e4a5b6029bfdf5f2364a28ff831e6ecbf5f
Jackson Quintero <codigoarchivo@gmail.com>
2022-12-27 20:19:13 -0400

    models/index.js: exportaciones

---
+++ models/index.js
@@ -0,0 +1,13 @@
+const Category = require("./category");
+const Role = require("./role");
+const Sever = require("./server");
+const Product = require("./product");
+const User = require("./user");
+
+module.exports = {
+  Category,
+  Role,
+  Sever,
+  Product,
+  User,
+};
